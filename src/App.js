5ba4ebaceda8513d6621c2843120754434ee01f2
mSenad <senad.metiljevic93@gmail.com>
2021-01-25 21:54:27 +0100

    repo added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { gsap } from "gsap";
 
 function App() {
+  const ITEM_SIZE = 150;
+  const [result, setResult] = useState(0);
+  const numbersWrapper = useRef();
+  const numbers = Array.from(new Array(100));
+  const counterItems = numbers.map((itm, index) => ({ id: index })).reverse();
+
+  const addResult = () => {
+    const resultUpdated = result + Math.floor(Math.random() * 3) + 1;
+    setResult(resultUpdated);
+  };
+
+  useEffect(() => {
+    gsap.to(numbersWrapper.current, { y: ITEM_SIZE * result });
+  }, [result]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="main-container__holder">
+          <div className="counter">
+            <div ref={numbersWrapper} className="counter__numbers">
+              {counterItems.map((itm, index) => (
+                <div key={index}>
+                  <span>{itm.id}</span>
+                </div>
+              ))}
+            </div>
+            <div className="counter__holder"></div>
+          </div>
+        </div>
+        <div className="button-wrapper">
+          <button type="button" onClick={addResult}>
+            Add random number
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
